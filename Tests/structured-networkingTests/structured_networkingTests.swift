e62718603f63aab086d23dc0a9b547e277f323ba
Kyle Newsome <kyle@pyrus.io>
2021-02-01 16:33:41 -0500

    platform support fix

--- Tests/structured-networkingTests/structured_networkingTests.swift
+++ Tests/structured-networkingTests/structured_networkingTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class structured_networkingTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(structured_networking().text, "Hello, World!")
+        XCTFail("write moar tests, bro")
     }
 
     static var allTests = [
